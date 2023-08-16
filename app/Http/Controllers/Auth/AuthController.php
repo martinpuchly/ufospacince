99283656cec28a1ac3f75e7feff22fd46554bac0
serifko <martin.puchly@gmail.com>
2023-08-16 09:28:55 +0200

    fb login msg

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
      
         Auth::login($user);
      
-        return redirect('/');
+        return redirect('/')->with('Prihlásenie prebehlo úspešne.');
     }
 
 
